3e859ecee13cec9a348c70f01a3845236249b129
Diananansubuga <117372394+Diananansubuga@users.noreply.github.com>
2023-09-18 14:04:41 +0300

    Add files via upload
    Write a program that reads a positive integer, n, from the user and then displays the
    sum of all of the integers from 1 to n. The sum of the first n positive integers can be
    computed using the formula:
    sum =
    (n)(n + 1)
    2

---
+++ sum of first n numbers day7.py
@@ -0,0 +1,10 @@
+# Write a program that reads a positive integer, n, from the user and then displays the
+# sum of all of the integers from 1 to n. The sum of the first n positive integers can be
+# computed using the formula:
+# sum =
+# (n)(n + 1)/2
+
+
+num=int(input("enter any positive number"))
+total=((num)*(num+1))/2
+print(total)
