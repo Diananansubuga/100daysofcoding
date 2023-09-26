a666c1f2879d3196068a0216923c2499171d9c76
Diana Clarissa Nansubuga <117372394+Diananansubuga@users.noreply.github.com>
2023-09-26 17:09:02 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+
+def multiply(num1,num2):
+    print(num1*num2)
+def divide(num1,num2):
+    print(num2/num1)
+def add(num1,num2):
+    print(num2+num1)
+def subtract(num1,num2):
+    print(num2-num1)
+
+# add(num1,num2)
+# subtract(num1,num2)
+# multiply()
+# divide()
